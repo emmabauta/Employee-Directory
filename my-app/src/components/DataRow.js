a3715e6f7d6b8ce240dd0e1d0d97df139a7905d5
DillEmma <emmabauta@gmail.com>
2021-02-27 21:25:59 -0500

    added table data

--- my-app/src/components/DataRow.js
+++ my-app/src/components/DataRow.js
@@ -1,30 +1,34 @@
 import React, { useContext } from "react";
-import DataContext from "../utils/DataContext";
+import DataContext from '../utils/DataContext';
 
-const DataRow = () => {
+function DataRow() {
     const { image, name, phoneNumber, email, dob } = useContext(DataContext);
-    return () => {
-                    <tr>
-                        <td data-th="Image">
-                            <img
-                                src={image}
-                                alt={"profile"}
-                                className="profile-pic"
-                            />
-                        </td>
-                        <td>
-                            <td data-th="Name" className="name">{name.first} {name.last}</td>
-                        </td>
-                        <td>
-                            <td data-th="Phone" className="phonenumber">{phoneNumber}</td>
-                        </td>
-                        <td>
-                            <td data-th="Email" className="email">{email}</td>
-                        </td>
-                        <td>
-                            <td data-th="DOB" className="birth">{dob}</td>
-                        </td>
-                    </tr>
-                }
-           }
+    return (
+    <tbody>
+        <tr>
+            <td data-th="Image">
+                <img
+                    src={image}
+                    alt={"profile"}
+                    className="profile-pic"
+                />
+            </td>
+            <td>
+                <td data-th="Name" className="name">{name.first} {name.last}</td>
+            </td>
+            <td>
+                <td data-th="Phone" className="phonenumber">{phoneNumber}</td>
+            </td>
+            <td>
+                <td data-th="Email" className="email">{email}</td>
+            </td>
+            <td>
+                <td data-th="DOB" className="birth">{dob}</td>
+            </td>
+        </tr>
+    </tbody>
+        
+    )
+}
+
 export default DataRow
